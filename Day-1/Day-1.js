f641c26d16ca78b5fccd0b5ba32043f08cbde652
kaveri koli <112814588+kaveri-01@users.noreply.github.com>
2026-08-19 22:36:38 +0530

    Day-1

---
+++ Day-1/Day-1.js
@@ -0,0 +1,24 @@
+// // let age = 25;
+// // let age = 20;{}
+// // let age = 17
+// // console.log(age);
+// // var age = 25;
+// // console.log(age);
+// // { block scope}
+
+
+// age define
+// navbar
+// home
+
+// var age = 10;
+// console.log(age);
+// // var a;
+// // console.log(a);
+// // a = 10;
+
+// // 
+
+// console.log("Hello")
+// console.log(10 + 10);
+console.log("hello")
---
+++ Day-1/Script.js
@@ -0,0 +1,9 @@
+console.log("Hello, JavaScript!");
+// console.log("I am learning JavaScript.");
+console.log(10 * 60);
+
+
+console.log("student information");
+console.log("Name : kaveri");
+console.log("Age : 25");
+console.log("Course : JavaScript");
