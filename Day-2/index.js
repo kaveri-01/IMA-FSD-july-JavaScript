2e529569a2bb347d4e86df5dcae119c091ecee94
kaveri koli <112814588+kaveri-01@users.noreply.github.com>
2026-08-21 22:21:46 +0530

    day-3

---
+++ Day-2/index.js
@@ -0,0 +1,63 @@
+// var age = 25;
+// var - you can reAssign and Redeclare
+
+
+// console.log("Rahul");
+// console.log("Rahul");
+// console.log("Rahul");
+// console.log("Rahul");
+
+// let name = "rahul";
+
+// console.log(name)
+// let - you can reclassign but you cannot redeclare
+// let age = 20;
+// //   age = 25;
+// console.log( typeof age)
+// const - you cannot reclassign or  redeclare
+// const age = 20;
+// age = 25
+// console.log(age);
+// "";
+// 23
+
+// let name = 'kaveri';
+// // let lastName = 'koli';
+
+// console.log( typeof name );
+
+// let isLoggedIn = true;
+// console.log(typeof isLoggedIn);
+
+
+// let name = null;
+// console.log( typeof name);
+
+// let fisrtnumber = 10;
+// let secondnumber = 20;
+// let total = fisrtnumber + secondnumber;
+// console.log(total);
+
+// const productNmae = "laptop";
+// const price = 20000;
+// const isAvailable = true;
+// let quantity = 4;
+
+// const totalPrice = price * quantity;
+
+// console.log(totalPrice);
+
+// let name = "Rahul";
+// let age = 25;
+// let isStudent = true;
+// let course = null
+
+// console.log(typeof name);
+// console.log(typeof age);
+// console.log(typeof isStudent);
+// console.log(typeof course);
+
+
+
+
+
---
+++ Day-3/index.js
@@ -0,0 +1,115 @@
+
+//     variable    operand   operator  operand
+// let num =       10         *        20 ;  operation
+// console.log(num)
+//         expression
+// result = 200
+
+// let a = 10;
+// let b = 20;
+// console.log(a + b)
+// let a = 10;
+// let b = 20;
+// console.log(a * b)
+// let a = 10;
+// let b = 20;
+// console.log(a - b)
+// let a = 10;
+// let b = 20;
+// console.log(a % b)
+// let a = 10;
+// let b = 20;
+// console.log(a / b)
+
+// Exponentiation
+// let total = 4 ** 6;
+// console.log(total)
+
+// let score = 10;
+// score  += 5
+// console.log(score);
+
+
+// Increment
+// let count = 10;
+// count ++
+// console.log(count);
+
+// Decrement
+// let count = 10;
+// count --
+// console.log(count);
+
+// comparative operator == :- checks value only
+// let a = 20;
+// let b = 20;
+// let compare = a == b;
+// console.log(compare);
+
+// comparative operator === :- checks value and data type
+// let a = "20";
+// let b = 20;
+// let compare = a === b;
+// console.log(compare);
+
+
+// less than
+// console.log (5 <= 5)
+
+// greater than 
+// console.log(5 >= 5)
+
+// console.log(10 !== 5)
+
+// console.log(true && false)
+
+// let age = 17;
+// let hasLicense = true;
+// console.log(age >= 18 && hasLicense)
+
+// let age = 17;
+//  let hasLicense = false;
+//  console.log(age >= 18 || hasLicense);
+
+// console.log(!false)
+
+// let userName = true;
+// let userpassword = false;
+// let loginSuccessfull = userName || userpassword;
+// console.log(loginSuccessfull);
+
+// let price = 1000;
+// let quantity = 7;
+// let total = price * quantity
+// let discount = 200;
+// let finalAmount = total - discount;
+// console.log(finalAmount);
+
+// let result = 10 + 5 * 2;
+// console.log(result);
+
+// let result = (10 + 5) * 2;
+// console.log(result);
+
+// let math = 40;
+// let science = 50;
+// let english = 60;
+
+// let totalmarks = math + science + english;
+// let average = totalmarks / 3
+// console.log("TotalMarks",totalmarks );
+// console.log("Average" ,average);
+
+
+let math = 40;
+let science = 50;
+let english = 60;
+
+
+let totalmarks = math + science + english;
+let average = totalmarks / 3;
+let result = average >= 40;
+
+console.log("Total Marks:", totalmarks); 
+console.log("Average:", average);
+console.log("Result:", result );
