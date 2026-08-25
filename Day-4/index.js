1179f89469f32cb1a243f336e5f60d55964e1a59
kaveri koli <112814588+kaveri-01@users.noreply.github.com>
2026-08-25 22:01:00 +0530

    day-4

---
+++ Day-4/index.js
@@ -0,0 +1,58 @@
+// let age = "kaveri";
+// let convertNumber = Boolean(age)
+// console.log(convertNumber);
+// console.log(typeof convertNumber);
+
+// console.log(Number("20"));
+
+// let age = 24;
+// let convertnumber = Boolean(age);
+// console.log(convertnumber);
+// console.log(typeof convertnumber)
+
+// console.log(Boolean(false));
+// console.log(Boolean(0));
+// console.log(Boolean(""));
+// console.log(Boolean(null));
+// console.log(Boolean(undefined));
+
+// console.log(Boolean(1));
+// console.log(Boolean(-1));
+// console.log(Boolean(""));
+// console.log(Boolean("0"));
+// console.log(Boolean([]));
+// console.log(Boolean({}));
+
+// let age = "25kaveri"
+// let convertNumber = Number(age)
+// console.log(convertNumber);
+
+// let firstName = "Rahul";
+// let lastName = "Sharma";
+
+// let fullName = firstName + " "  + lastName;
+
+// console.log(fullName);
+
+// console.log(10 != "10");
+// console.log(10 !== "10");
+// let age = "20";
+// let convertNumber = Number(age)
+// console.log (convertNumber + 5)
+
+// let price = "1500";
+// let quantity = 2;
+// console.log(price * quantity);
+console.log("10" + 5);
+console.log("10" - 5);
+console.log("10" * 2);
+
+console.log("20" / 2);
+
+console.log(10 == "10"); 
+console.log(10 === "10");
+
+console.log(Boolean(0));
+console.log(Boolean(1));
+console.log(Boolean(" "));
+console.log(Boolean("Hello"));
