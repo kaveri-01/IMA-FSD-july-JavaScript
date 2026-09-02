34c8e4537d5cb667ae62a3d8bec41b51eae7ca8a
kaveri koli <112814588+kaveri-01@users.noreply.github.com>
2026-09-02 21:54:41 +0530

    day-7

---
+++ Day-5/Day-6/Day-7/index.js
@@ -0,0 +1,32 @@
+// function sayHello (){
+//     console.log("Hello")
+// }
+// sayHello();
+
+// function sayName (name){
+//     console.log("Hello" + " " +  name)
+// }
+// sayName("Kaveri");
+
+
+// function add (a ,b, c){
+//     console.log(a + b + c)
+// }
+// add (10, 20, 30);
+
+// function add(a, b) {
+// return a + b;
+// }
+// const result = add(10, 20);
+// console.log(result);
+
+// function calculateTotal(price, quantity) {
+// return price * quantity;
+// }
+// const total = calculateTotal(500, 3);
+// console.log(total);
+
+// function multiply(a, b) {
+// return a * b;
+// }
+// console.log(multiply(4, 5));
