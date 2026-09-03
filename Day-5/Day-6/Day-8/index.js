7ef1351cb7e664c7a3f3854008f5978226c4dcd5
kaveri koli <112814588+kaveri-01@users.noreply.github.com>
2026-09-03 22:07:19 +0530

    day-8

---
+++ Day-5/Day-6/Day-8/index.js
@@ -0,0 +1,73 @@
+// function add (a,b){
+//     return a+b
+// }
+// console.log(add(10,20));
+
+// const add = function (a,b){
+//     return a+b
+// }
+// console.log(add(10,20));
+
+// const add = (a, b)=>{
+//  return a+b
+// }
+// console.log(add(10, 20));
+
+// const greet = (name) => {
+//    console.log("Hello " + name)
+// }
+// greet("Kaveri")
+
+// const add = (a,b) => a+b
+// console.log(add(10,20))
+
+// const square = num => num * num;
+// console.log(square(5));
+
+// const greet = name =>   `Hello ${name}`;
+// console.log(greet("Kaveri"));
+
+
+// const fruits = ["Banana", "Apple", "Orange", "Papaya"];
+// fruits[1] = "Kiwi"
+// console.log(fruits);
+
+
+// Push();
+// adds element at last 
+// const fruits = ["Apple", "Mango"];
+// fruits.push("Banana");
+// console.log(fruits);
+
+// pop()
+// pop delete last element
+// const fruits = ["Apple", "Mango"];
+// fruits.pop();
+// console.log(fruits);
+
+// shift()
+// delete first element
+// const fruits = ["Apple", "Mango"];
+// fruits.shift();
+// console.log(fruits);
+
+// unshift()
+// add element at first
+// const fruits = ["Apple", "Mango"];
+// fruits.unshift("Banana");
+// console.log(fruits);
+// const num = [1,2,3,4,5]
+// const result = [];
+// for (let i = 0; i < num.length; i++){
+//     result.push(num[i] * 2);
+// }
+// console.log(result);
+
+const students = [
+"Rahul",
+"Amit",
+"Priya",
+"Sneha"
+];
+console.log(students.length);
+console.log(students[0]);
