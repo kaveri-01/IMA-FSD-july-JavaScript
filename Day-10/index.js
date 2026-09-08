a284d3ed15031d0989f96087c9b615d72360d2ea
kaveri koli <112814588+kaveri-01@users.noreply.github.com>
2026-09-08 21:56:08 +0530

    day-10

---
+++ Day-10/index.js
@@ -0,0 +1,90 @@
+// const student = {
+//     name : "kaveri",
+//     age : "25",
+//     city: "mumbai"
+// };
+// console.log(student)
+
+// const car = {
+//     brand : "Toyota",
+//     model : "Fortuner",
+//     year : 2025,
+//     price :"25 Lakhs"
+// };
+// console.log(car.brand);
+// console.log(car["model"]);
+
+// const student = {
+//     name : "kaveri",
+//     age : "25"
+// }
+// student.age = 20;
+// console.log(student);
+
+// const student = {
+//     name : "kaveri",
+//     age : "25"
+
+// };
+// student.course = "js";
+// console.log(student);
+
+// const employee = {
+//     name : "priya",
+//     department : "it"
+// }
+// employee.salary = 50000;
+// console.log(employee);
+
+// const employee = {
+//     name : "priya",
+//     department : "it"
+// }
+// delete employee.name
+// console.log(employee);
+
+// const students = {
+//     name : "kaveri",
+//     age : 25,
+
+//     address : {
+//         city: "mumbai",
+//         state: "maharashtra"
+//     }
+// }
+// delete students.address.state
+// students.name = "shruti"
+// console.log(students);
+
+// const employee = {
+//     name : "kaveri",
+
+//     company : {
+//         name : "Tech",
+
+//     location : {
+//         city : "mumbai",
+//         country : "india"
+//     }
+//     }
+// };
+// console.log(employee.company.location.country);
+
+// const product = {
+// name: "Laptop",
+// price: 55000,
+// category: "Electronics",
+// available: true
+// };
+// console.log(product.name);
+// console.log(product.price);
+
+const user = {
+name: "Rahul",
+age: 25,
+city: "Pune"
+};
+user.age = 20;
+user.email = "rahul@123"
+delete user.city
+console.log(user);
